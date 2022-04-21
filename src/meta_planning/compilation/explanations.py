e3633d53ac72ace00ea525e92f4344d82e6ce15c
Natan Junges <natanajunges@gmail.com>
2022-04-21 22:42:40 +0000

    Update explanations.py

--- src/meta_planning/compilation/explanations.py
+++ src/meta_planning/compilation/explanations.py
@@ -1,7 +1,7 @@
 
 class Explanation(object):
 
-    def __init__(self, plan, observation, inferred_trajectory):
+    def __init__(self, plan, observation, lifted_inferred_trajectory):
         self.plan = plan
         self.observation = observation
-        self.inferred_trajectory = inferred_trajectory
+        self.lifted_inferred_trajectory = lifted_inferred_trajectory

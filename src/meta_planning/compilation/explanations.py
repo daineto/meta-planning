321d527c506fbcfb6a28c2cd32e2a4085290ebed
Natan Junges <natanajunges@gmail.com>
2022-04-21 03:05:53 +0000

    Add trajectory to Explanation

--- src/meta_planning/compilation/explanations.py
+++ src/meta_planning/compilation/explanations.py
@@ -1,6 +1,7 @@
 
 class Explanation(object):
 
-    def __init__(self, plan, observation):
+    def __init__(self, plan, observation, trajectory):
         self.plan = plan
         self.observation = observation
+        self.trajectory = trajectory

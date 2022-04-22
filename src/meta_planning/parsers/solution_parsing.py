fe1747339e0c4202ed3ae13abed0d653dffef12f
Natan Junges <natanajunges@gmail.com>
2022-04-22 00:06:04 +0000

    Learn preconditions that do not appear as negative effects

--- src/meta_planning/parsers/solution_parsing.py
+++ src/meta_planning/parsers/solution_parsing.py
@@ -108,7 +108,7 @@ def parse_plan(plan_file):
 
 
 
-def parse_solution(solution_file, initial_model, observations, known_model):
+def parse_solution(solution_file, initial_model, observations, known_model, lifted_inferred_trajectories= None):
 
     observations_contain_actions = any([o.has_actions() for o in observations])
 
@@ -189,5 +189,38 @@ def parse_solution(solution_file, initial_model, observations, known_model):
     learned_model = build_model(pres, effs, initial_model)
 
     explanations += build_explanations(regular_actions, cuts, observations, learned_model)
+    
+    if lifted_inferred_trajectories is None:
+        lifted_inferred_trajectories = []
+
+    lifted_inferred_trajectories += [explanation.lifted_inferred_trajectory for explanation in explanations]
+    pre_states = {}
+
+    for lifted_inferred_trajectory in lifted_inferred_trajectories:
+        for pre_state in lifted_inferred_trajectory.states:
+            action = pre_state.next_action
+
+            if action is not None:
+                pre_states_list = pre_states.get(action.name, [])
+                pre_states_list.append(set(pre_state.literals))
+                pre_states[action.name] = pre_states_list
+
+    action_names = [a.name for a in learned_model.schemata]
+
+    for k, v in pre_states.items():
+        new_preconditions = v[0]
+
+        for pre_state in v[1:]:
+            new_preconditions = new_preconditions.intersection(pre_state)
+
+        if len(new_preconditions) > 0:
+            new_preconditions = sorted(new_preconditions)
+            indexa = action_names.index(k)
+            learned_pres = list(learned_model.schemata[indexa].precondition.parts)
+            new_preconditions = [pre for pre in new_preconditions if pre not in learned_pres]
+
+            if len(new_preconditions) > 0:
+                learned_model.schemata[indexa].precondition = Conjunction(learned_pres + new_preconditions)
+                edition_distance += len(new_preconditions)
 
     return FoundSolution(plan, learned_model, edition_distance, explanations)
